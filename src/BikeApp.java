cee6540746882625744ecaddd34eec4dde344eea
JosephEmeka <josephemekaonah@gmail.com>
2024-02-05 00:43:13 +0100

    bike app version 1.0
    This version has on acceleration working . Deceleration will be released soon.

---
+++ src/BikeApp.java
@@ -0,0 +1,59 @@
+public class BikeApp {
+    private boolean bikeIsOn;
+    private int gear;
+
+    private int speed;
+
+    public BikeApp() {
+        this.bikeIsOn = false;
+        this.gear = 0;
+        this.speed = 0;
+    }
+
+
+    public boolean itIsOff() {
+        return bikeIsOn;
+    }
+
+    public void turnBikeOn() {
+        this.bikeIsOn = true;
+    }
+
+    public void turnBikeOff() {
+        this.bikeIsOn = false;
+    }
+
+    public void setGear(int gear) {
+       this.gear = gear;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+
+    public int getSpeed() {
+        return speed;
+    }
+
+
+    public int accelerate() {
+            if (bikeIsOn == true) {
+                if (gear == 1 && speed <= 20) {
+                    speed += 1;
+                } else if (gear == 2 && speed <= 30) {
+                    speed += 2;
+                } else if (gear == 3 && speed <= 40) {
+                    speed += 3;
+                }
+                else if (gear == 4 && speed > 40) {
+                    speed += 4;
+                }
+                return speed;
+            }
+            else {
+
+                return 0;
+            }
+        }
+}
---
+++ test/BikeAppTest.java
@@ -0,0 +1,72 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class BikeAppTest {
+
+
+        @Test
+        public void testThatBikeCanBeTurnedOn(){
+            BikeApp myBikeApp = new BikeApp();
+            assertFalse(myBikeApp.itIsOff());
+            myBikeApp.turnBikeOn();
+            assertTrue(myBikeApp.itIsOff());
+        }
+
+        @Test
+        public void testThatBikeCanBeTurnedOff(){
+            BikeApp myBikeApp = new BikeApp();
+            myBikeApp.turnBikeOn();
+            myBikeApp.turnBikeOff();
+            assertFalse(myBikeApp.itIsOff());
+        }
+
+    @Test
+    public void testThatBikeCannotAccelerateWhenBikeIsOff(){
+        BikeApp myBikeApp = new BikeApp();
+        myBikeApp.itIsOff();
+        assertEquals(0, myBikeApp.accelerate());
+
+    }
+
+    @Test
+        public void testThatBikeCanAccelerate(){
+            BikeApp myBikeApp = new BikeApp();
+            myBikeApp.turnBikeOn();
+            myBikeApp.setGear(1);
+            myBikeApp.setSpeed(15);
+            assertEquals(16,myBikeApp.accelerate());
+            myBikeApp.setGear(2);
+            myBikeApp.setSpeed(24);
+            assertEquals(26, myBikeApp.accelerate());
+            myBikeApp.setGear(3);
+            myBikeApp.setSpeed(35);
+            assertEquals(38, myBikeApp.accelerate());
+            myBikeApp.setGear(4);
+            myBikeApp.setSpeed(44);
+            assertEquals(48, myBikeApp.accelerate());
+
+        }
+
+
+
+    @Test
+    public void testThatBikeCanDecelerate(){
+
+
+
+    }
+
+    @Test
+    public void testThatBikeCannotDecelerateWhenBikeIsOff(){
+        BikeApp myBikeApp = new BikeApp();
+        myBikeApp.itIsOff();
+        assertEquals(0, myBikeApp.decelerate());
+
+
+    }
+
+
+}
